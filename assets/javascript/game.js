c665b891c9ecabefdd6d62409d917c0feb63502a
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2018-12-14 23:34:25 -0500

    got main, most difficult part of game working.  Still need to limit number of guesses and other small changes.  Also needs styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,16 @@ window.onload = function() {
   var targetWordList = ["Williamsburg", "Tumblr", "gastropub", "mumblecore", "fashion", "brunch", "vinyl", "tousled", "dreamcatcher", "letterpress", "distillery", "keffiyeh", "banjo", "Kickstarter", "asymmetrical", "denim", "messenger", "fingerstache", "Schlitz", "readymade", "YOLO", "bitters", "aesthetic", "typewriter", "flexitarian", "selfies", "sustainable", "Bushwick", "mixtape", "artisan", "wayfarers", "normcore", "meggings", "salvia", "Pinterest", "skateboard", "Etsy", "hashtag", "retro", "iPhone", "sriracha", "forage", "polaroid", "organic", "locavore", "keytar", "plaid", "authentic", "mustache", "ennui", "quinoa", "umami", "biodiesel", "ugh", "disrupt", "meh", "Banksy", "Pitchfork", "Helvetica", "cliche", "sartorial", "irony", "squid", "actually", "Brooklyn", "gentrify", "Austin", "twee", "DIY", "semiotics", "kitsch", "bespoke", "pickled", "cred", "viral", "Portland", "heirloom", "freegan", "seitan", "hoodie", "whatever", "cardigan", "Thundercats", "PBR", "literally", "pug", "leggings", "scenester", "tofu", "beard", "paleo", "ethical", "chia", "tattooed", "Vice",];
 
   var lettersGuessed
+  var wins = 0;
+  
 
   // setting up variables for html ids
   var wordHTM = document.getElementById("word");
   var lettersGuessedHTM = document.getElementById("lettersGuessed");
   var guessesRemainingHTM = document.getElementById("guessesRemaining");
   var winsHTM = document.getElementById("wins");
+
+  winsHTM.textContent = 0;
 
 
   // set random targetWord and array of letters from that word
@@ -24,9 +28,14 @@ window.onload = function() {
   console.log(targetWordLetters);
 
   // populating html word spot with underscores
-  for (i=0; i<targetWordLetters.length; i++) {
-    wordHTM.append("_ ");
+  var temporaryWord = ["_"];
+  for (i=0; i<(targetWordLetters.length-1); i++) {
+    temporaryWord.push("_");
   }
+  console.log (temporaryWord);
+    for (i=0; i<temporaryWord.length; i++) {
+    wordHTM.append(temporaryWord[i]);
+    }
   
 
   // get user input, record in guess, see if it is in target word.
@@ -35,13 +44,50 @@ window.onload = function() {
     console.log("user guessed " + guess);
     var isInWord = targetWordLetters.includes(guess);
     console.log("guess is in word: "+isInWord);
+    var letterCounter = targetWord.length;
+    console.log("word is "+letterCounter+" letters");
+
+  // set up if/thens to respond to correct and incorrect guesses
+    if (isInWord) {
+      console.log("it is in word");
+      
+      // thank you stack overflow for the following, pulls positions of any guessed word into index array
+      var index = [];
+      for (var i=0; i<targetWordLetters.length; i++){
+        if (targetWordLetters[i] === guess) index.push(i);
+      }
+      console.log(index);
+
+      // replace underscores with appropriate letters in wordHTM
+      for (i=0; i<index.length; i++) {
+        l=index[i];
+        console.log(l);
+        temporaryWord[l] = targetWordLetters[l];
+        console.log(temporaryWord);
+      }
+        var underscoreWord = "";
+        for (i=0; i<temporaryWord.length; i++){
+          underscoreWord += temporaryWord[i];
+        }
+      
+        wordHTM.innerHTML = underscoreWord;
+      
+        var wordNotDone = underscoreWord.includes("_");
+        console.log("word is not done: "+wordNotDone);
+        if (wordNotDone === false) {
+          wins++;
+          winsHTM.textContent = wins;
+        }
+        
+
+      }
+    }
+
   }
 
-  
 
 
 
 
 
 
-}
